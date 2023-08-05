e233ae44568b827f1e590f5839d8231749766d9f
Leila Ibañez <133930984+Leila-ibz@users.noreply.github.com>
2023-08-05 16:17:13 -0300

    .

--- blog/configuraciones/settings.py
+++ blog/configuraciones/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD': '12345',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306'
     }
