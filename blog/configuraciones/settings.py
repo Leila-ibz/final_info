686dbed45b6b0570079b02d967b6502c4d9cfa89
neko27 <dackfort.ao@gmail.com>
2023-08-05 17:36:07 -0300

    Actualice mi rama

--- blog/configuraciones/settings.py
+++ blog/configuraciones/settings.py
@@ -131,14 +131,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (                                                                                    
-    os.path.join(os.path.dirname(BASE_DIR), 'static'),
-)
+STATICFILES_DIRS = [os.path.join(os.path.dirname(BASE_DIR),'static'),]                                                                                    
+    
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = (
-    os.path.join(os.path.dirname(BASE_DIR), 'media'),
-)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (os.path.join(os.path.dirname(BASE_DIR),'media'),)
+    
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
